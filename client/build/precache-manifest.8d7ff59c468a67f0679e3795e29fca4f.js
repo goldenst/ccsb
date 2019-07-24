812b8a447e34551f0afa8e262d4b1d64b43c9939
MSI\jeff <hatcher28@me.com>
2019-07-23 23:36:19 -0700

    build file

---
+++ client/build/precache-manifest.8d7ff59c468a67f0679e3795e29fca4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d2e7b2a1d8fc01cfc0bc91c207f725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e0890353c18d2426d6e",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "878425dd0e11f0fe368f",
+    "url": "/static/css/main.b9a0fee2.chunk.css"
+  },
+  {
+    "revision": "0e0890353c18d2426d6e",
+    "url": "/static/js/2.6faf08ee.chunk.js"
+  },
+  {
+    "revision": "878425dd0e11f0fe368f",
+    "url": "/static/js/main.dbf60a9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
